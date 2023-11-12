074a53794292f422822ea9240d115fafd1092a18
eshal5 <122152840+eshal5@users.noreply.github.com>
2023-11-12 14:04:06 +0500

    Update cell.cpp

--- cell.cpp
+++ cell.cpp
@@ -16,3 +16,38 @@ Cell::Cell(int i, int j)
     westwalls=true;
     northwalls=true;
     southwalls=true;}
+
+ Cell& Cell::operator=(const Cell& c) const {
+    x = c.x;
+    y = c.y;
+    visited = c.visited;
+    eastwalls=c.eastwalls;
+    westwalls=c.westwalls;
+    northwalls=c.northwalls;
+    southwalls=c.southwalls;
+
+    return *this;
+}
+
+std::string Cell::direction(const Cell& other) {
+    if (x == other.x && y + 1 == other.y)
+        return "E";
+    if (x == other.x && y - 1 == other.y)
+        return "W";
+    if (x - 1 == other.x && y == other.y)
+        return "N";
+    if (x + 1 == other.x && y == other.y)
+        return "S";
+    return "";
+}
+std::string Cell::directionopposite(std::string direct) {
+    if (direct=="W")
+        {return "E";}
+    else if (direct=="E")
+        {return "W";}
+    else if (direct=="S")
+        {return "N";}
+    else if (direct=="N")
+        {return "S";}
+    return "";
+}
