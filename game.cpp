438b85b9f74faee2dd5bd185cf3bdc1a1003e248
IfrahC <122103840+IfrahC@users.noreply.github.com>
2023-11-13 16:08:38 +0500

    Added commented code
    Using past HW code as refrence

--- game.cpp
+++ game.cpp
@@ -158,3 +158,48 @@ void Game::run( )
 	}
 			
 }
+
+// const int xJump=220, yJump=170;
+
+// SDL_Rect bg={0,0,800,600};
+// SDL_Rect bgSrc={0,369,778,586};
+// SDL_Rect alexSrc={779, 0, 294, 848};
+// SDL_Rect alexMover={510, 150, 25, 75};
+
+// //Screen dimension constants
+// const int SCREEN_WIDTH = 800;
+// const int SCREEN_HEIGHT = 600;
+
+// void moveSouth(){ 
+//     if (alexMover.y+yJump < SCREEN_HEIGHT)alexMover.y+=yJump;    
+// }
+// void moveNorth(){
+// 	if (alexMover.y-yJump > 0)alexMover.y-=yJump;
+// 	else alexMover.y = 0;
+// }
+// void moveEast(){
+// 	if (alexMover.x+xJump < SCREEN_WIDTH)alexMover.x+=xJump;
+// }
+// void moveWest(){
+// 	if (alexMover.x-xJump > 0)alexMover.x-=xJump;
+// }
+
+//Moving the character
+// void moveAlex(SDL_Renderer* gRenderer, SDL_Texture* assets, SDL_Keycode key){
+//     if (key == SDLK_UP){
+//         makeMove("North");
+//     }
+//     else if (key == SDLK_DOWN) {
+//         makeMove("South");
+//     }
+//     else if (key == SDLK_RIGHT) {
+//         makeMove("East");
+//     }
+//     else if (key == SDLK_LEFT) {
+//         makeMove("West");
+//     }
+//     else {
+//         std::cout<<"hello";
+//     }
+//     update(gRenderer, assets);
+// }
