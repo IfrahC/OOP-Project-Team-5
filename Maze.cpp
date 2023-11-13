b6e5a95646eb4cdbe7a3f6d5ade6795d29b87f1c
eshal5 <122152840+eshal5@users.noreply.github.com>
2023-11-13 23:24:02 +0500

    Maze.cpp

--- Maze.cpp
+++ Maze.cpp
@@ -24,7 +24,6 @@ class Maze {
 };
 
 Maze::Maze() {
-    std::srand(static_cast<unsigned int>(std::time(nullptr)));
 
     // Define the fixed starting and exit points
     start_x = 0;
@@ -41,26 +40,26 @@ Maze::Maze() {
 }
 
 std::vector<std::vector<int>> Maze::get_all_neighbors(int i, int j) {
-    std::vector<int> co_ordinates; //make vector here
+    std::vector<int> co_ordinates; 
     std::vector<std::vector<int>> valid_neighbors;
 
     if ((i >= 0 && i < N) && (j + 1 >= 0 && j + 1 < N) && (!grids[i][j + 1].visited)) {
-        //co_ordinates = std::make_tuple(i, j + 1);
+      
         co_ordinates={i,j+1};
         valid_neighbors.push_back(co_ordinates);
     }
     if ((i >= 0 && i < N) && (j - 1 >= 0 && j - 1 < N) && (!grids[i][j - 1].visited)) {
-        //co_ordinates = std::make_tuple(i, j - 1);
+     
         co_ordinates={i,j-1};
         valid_neighbors.push_back(co_ordinates);
     }
     if ((i - 1 >= 0 && i - 1 < N) && (j >= 0 && j < N) && (!grids[i - 1][j].visited)) {
-        //co_ordinates = std::make_tuple(i - 1, j);
+      
         co_ordinates={i-1,j};
         valid_neighbors.push_back(co_ordinates);
     }
     if ((i + 1 >= 0 && i + 1 < N) && (j >= 0 && j < N) && (!grids[i + 1][j].visited)) {
-       // co_ordinates = std::make_tuple(i + 1, j);
+ 
         co_ordinates={i+1,j};
         valid_neighbors.push_back(co_ordinates);
     }
