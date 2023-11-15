3d7a1987c938e2b2081709514142d3f51a649b8d
IfrahC <122103840+IfrahC@users.noreply.github.com>
2023-11-15 13:23:46 +0500

    Update collectables.cpp

--- collectables.cpp
+++ collectables.cpp
@@ -1,45 +1,39 @@
-#include<iostream>
+#include <iostream>
 #include "collectables.hpp"
 #include "Score.cpp"
 #include <vector>
 using namespace std;
 
 // Define a Collectible class
-class Collectible {
-    public:
-        SDL_Texture* texture;
-        SDL_Rect position;
-        //pure virtual function score incrementation
-        virtual int updateScore(Score& score) = 0;
+class Collectible
+{
+public:
+    SDL_Rect position;
 
-        // Load the image into the texture
-        void loadTexture(SDL_Renderer* renderer, const char* path) {
-            SDL_Surface* surface = IMG_Load(path);
-            texture = SDL_CreateTextureFromSurface(renderer, surface);
-            SDL_FreeSurface(surface);
-        }
+    // pure virtual function score incrementation
+    virtual int updateScore(Score &score) = 0;
 
-        // Render the collectible object
-        void render(SDL_Renderer* renderer) {
-            SDL_RenderCopy(renderer, texture, NULL, &position);
-        }
-
-        // Check for collision with the player
-        bool checkCollision(SDL_Rect player) {
-            return SDL_HasIntersection(&position, &player);
-        }
+    // Check for collision with the player
+    virtual bool checkCollision(SDL_Rect player)
+    {
+        return SDL_HasIntersection(&position, &player);
+    }
 };
 
-class Coin : public Collectible {
-    public:
-        int updateScore(Score& score) override {
-            ++score;
-        }
+class Coin : public Collectible
+{
+public:
+    int updateScore(Score &score) override
+    {
+        ++score;
+    }
 };
 
-class Diamond : public Collectible {
-    public:
-        int updateScore(Score& score) override {
-            score+=5;
-        }
+class Diamond : public Collectible
+{
+public:
+    int updateScore(Score &score) override
+    {
+        score += 5;
+    }
 };
