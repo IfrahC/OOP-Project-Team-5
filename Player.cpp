7557afeb168939b5ec1f0c42b9f47cd9c0eee841
eshal5 <122152840+eshal5@users.noreply.github.com>
2023-11-19 19:04:50 +0500

    Create Player.cpp

---
+++ Player.cpp
@@ -0,0 +1,41 @@
+// Player.cpp
+#pragma once
+#include "Player.hpp"
+
+void Player::addPlayer(SDL_Renderer* renderer, std::string mtexturePath, int x, int y, int width, int height,int mazesize) {
+    mover = new SDL_Rect{x, y, width, height};
+    texturePath=mtexturePath;
+    playertexture.texture=playertexture.load(renderer, texturePath);
+    xJump = SCREEN_HEIGHT/mazesize; 
+    yJump = SCREEN_HEIGHT/mazesize; 
+    curr_x=mazesize-1;
+    curr_y=mazesize-1;
+
+}
+
+Player::~Player() {
+    delete mover;
+    mover=nullptr;
+}
+
+void Player::moveSouth() {
+    if (mover->y + yJump < SCREEN_HEIGHT)
+        mover->y += yJump;
+}
+
+void Player::moveNorth() {
+    if (mover->y - yJump > 0)
+        mover->y -= yJump;
+    else
+        mover->y = 0;
+}
+
+void Player::moveEast() {
+    if (mover->x + xJump < SCREEN_WIDTH)
+        mover->x += xJump;
+}
+
+void Player::moveWest() {
+    if (mover->x - xJump > 0)
+        mover->x -= xJump;
+}
