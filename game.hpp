eec69d7d581fec0ea53ee895347e2c587fb2c33d
eshal5 <122152840+eshal5@users.noreply.github.com>
2023-11-19 18:59:19 +0500

    Update game.hpp

--- game.hpp
+++ game.hpp
@@ -1,35 +1,47 @@
 // game.hpp
-#ifndef GAME_H
-#define GAME_H
 
-#include "include/SDL2/SDL.h"
+#pragma once
+#include <SDL.h>
+  #include <SDL_image.h>
+  #include <SDL_ttf.h>
+
+#include "Cell.hpp"
 #include "Maze.h"
+
 #include "Texture.hpp"
-#include "include/SDL2/SDL_image.h"
+
 #include <stdio.h>
 #include <iostream>
 #include <string>
 #include <stdlib.h>
 #include <time.h>
-
+#include "Score.hpp"
+#ifndef GAME_HPP
+#define GAME_HPP
 class Game {
  
     const int SCREEN_WIDTH = 800;
     const int SCREEN_HEIGHT = 800;
-
+    Score points;
     // The window we'll be rendering to
     SDL_Window *gWindow = NULL;
     // The window renderer
     SDL_Renderer *gRenderer = NULL;
 
+ 
     Maze* generator;
-    int mazeSize;
-    
+    /*SDL_Texture* titleTexture;  
+    SDL_Texture* mazebgTexture;*/
     Texture titleTexture;
     Texture mazebgTexture;
-
+    Texture selectplayer;
+    //
+    Texture vwall;
+    Texture hwall;
+    Texture boy;
+    Texture girl;
+    
 public:
-    Game(int size);
     bool init();
     bool loadMedia();
     void close();
@@ -37,5 +49,5 @@ public:
     void run();
 };
 
-#endif // GAME_H
+#endif // GAME_HPP
 
