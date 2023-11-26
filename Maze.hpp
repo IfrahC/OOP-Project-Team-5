0c5d95d58bb623cb0702fb2e8372a68f9038cde4
eshal5 <122152840+eshal5@users.noreply.github.com>
2023-11-26 20:45:21 +0500

    Update and rename Maze.h to Maze.hpp

--- Maze.hpp
+++ Maze.hpp
@@ -1,6 +1,6 @@
 #pragma once
-#ifndef MAZE_H
-#define MAZE_H
+#ifndef MAZE_HPP
+#define MAZE_HPP
 #include "Texture.hpp"
 #include "Cell.hpp"
 #include <stack>
@@ -13,8 +13,9 @@
 class Maze {
     public:
    
-    Maze(int m_gridSize, int windowWidth, int windowHeight, SDL_Renderer* m_renderer);//constructor 
-     std::vector<std::vector< int>> get_all_neighbors(int i, int j); //function to get neighbors of the cell
+    Maze();//constructor 
+    void gridmaker(int m_gridSize, int windowWidth,int windowHeight, SDL_Renderer* m_renderer); //a function populates the grid with cells
+    std::vector<std::vector< int>> get_all_neighbors(int i, int j); //function to get neighbors of the cell
     void generate_maze(); //function to generate maze based on backtracking algo
 
     int start_x; // X-coordinate of the starting point 
@@ -25,13 +26,14 @@ class Maze {
 
     int gridSize; //size of maze
     ~Maze();
+    std::vector<std::vector<int>> list; //a list that stores the i,j position of the cells which have either a coin or a diamond
+    SDL_Renderer* renderer; 
     std::vector<std::vector<Cell>> grids; //2d vector representing maze grid
-    SDL_Renderer* renderer;
     int scale; //The scale determines how much space each cell occupies in both the horizontal and vertical directions based on window size
-    void DrawMaze(/*SDL_Color color*/);
-    void placeCollectibles();
-    void drawitems();
+    void DrawMaze(); //draws the maze by calling draw cell function of cell class
+    void placeCollectibles(); //randomly places the coin and diamonds in the grid
+    void drawitems(); //draws the randomly placed collectibles using the list
   
 };
-#endif // MAZE_H
+#endif // MAZE_HPP
 
