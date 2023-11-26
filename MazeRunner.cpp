94b5675d0f0c99e44bfa3d6064aacf1d77099a0d
eshal5 <122152840+eshal5@users.noreply.github.com>
2023-11-26 20:59:24 +0500

    Update MazeRunner.cpp

--- MazeRunner.cpp
+++ MazeRunner.cpp
@@ -1,8 +1,8 @@
 #pragma once
 #include <iostream>
 #include "MazeRunner.hpp"
-#include "mover.hpp"
-#include "Maze.h"
+#include "GameManager.hpp"
+#include "Maze.hpp"
 #include "game.hpp"
 #include "Player.hpp"
 
@@ -10,50 +10,53 @@
 
 
 std::string makeMove(string direction,Maze* generator,Player& boy,Score& points,std::string gameState){
-	if (direction == "East")
+    
+	if (direction == "East") //if the playe is to be moved in east direction
 	{ 
-       if (generator->grids[boy.curr_x][boy.curr_y].eastwalls==false){
-           if (boy.curr_x==0 && boy.curr_y==0){
+       if (generator->grids[boy.curr_x][boy.curr_y].eastwalls==false){ //if there is no wall
+           if (boy.curr_x==0 && boy.curr_y==0){ //if its the destination gamestate won
                gameState="WON";
            }
-          else{
+          else{ //otherwise player will be moved east
            boy.moveEast();
-           boy.curr_x+=1;}
+           boy.curr_x+=1;} //current x position of player will be incremented by 1
        }
 
       
       
         
 	}	
-	else if (direction == "West"){
-        if (generator->grids[boy.curr_x][boy.curr_y].westwalls==false){
-            if (boy.curr_x==0 && boy.curr_y==0){
+	else if (direction == "West"){ //if the player is to be moved in west direction
+        if (generator->grids[boy.curr_x][boy.curr_y].westwalls==false){ //if there is no wall
+            if (boy.curr_x==0 && boy.curr_y==0){ //if its the destination gamestate won
                gameState="WON";
-           }else{
+           }else{ //otherwise player will be moved west
            boy.moveWest();
-           boy.curr_x-=1;}
-       }
+           boy.curr_x-=1; //current x position of player will be decremented by 1
+           
+       }}
         
     }
 	
-	else if (direction == "North"){
-       if (generator->grids[boy.curr_x][boy.curr_y].northwalls==false){
-        if (boy.curr_x==0 && boy.curr_y==0){
+	else if (direction == "North"){//if the player is to be moved in north direction
+       if (generator->grids[boy.curr_x][boy.curr_y].northwalls==false){ //if there is no wall
+        if (boy.curr_x==0 && boy.curr_y==0){ //if its the destination gamestate won
                gameState="WON";
-           }else{
+           }else{  //otherwise player will be moved north
            boy.moveNorth();
-           boy.curr_y-=1;}
+           boy.curr_y-=1;} //current y position of player will be decremented by 1
        }
         
     }
-	else if (direction == "South"){
-        if (generator->grids[boy.curr_x][boy.curr_y].southwalls==false){
-            if (boy.curr_x==0 && boy.curr_y==0){
+	else if (direction == "South"){//if the player is to be moved in south direction
+        if (generator->grids[boy.curr_x][boy.curr_y].southwalls==false){ //if there is no wall
+            if (boy.curr_x==0 && boy.curr_y==0){ //if its the destination gamestate won
                gameState="WON";
-           }else{
+           }else{  //otherwise player will be moved south
             boy.moveSouth();
-           boy.curr_y+=1;}
+           boy.curr_y+=1;} //current y position of player will be incremented by 1
        }}
+       
     return gameState;   
 }
 
