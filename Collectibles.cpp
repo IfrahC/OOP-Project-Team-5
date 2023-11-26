f1e1d03fb0cd5d8c1cf86d1ab09172239a5fd0b9
eshal5 <122152840+eshal5@users.noreply.github.com>
2023-11-26 20:06:09 +0500

    Update Collectibles.cpp

--- Collectibles.cpp
+++ Collectibles.cpp
@@ -3,10 +3,10 @@
 Collectible::~Collectible()=default;
 Coin::Coin(){ };
 void Coin::collect(Score &score){
-      score.incrementscore(2)  ;
+      score.incrementscore(2)  ; //increments two in the overall score if a coin is collected
 }
 void Diamond::collect(Score &score){
-        score.incrementscore(5);
+        score.incrementscore(5); //increments 5 in the overall score if a diamond is collected
 }
 
 Diamond::Diamond(){};
