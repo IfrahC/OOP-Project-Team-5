9ac2dd9e35ada522f2cf01af69ddf27e2cceefa3
eshal5 <122152840+eshal5@users.noreply.github.com>
2023-11-26 21:08:24 +0500

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -3,12 +3,13 @@
 #include "Player.hpp"
 
 void Player::addPlayer(SDL_Renderer* renderer, std::string mtexturePath, int x, int y, int width, int height,int mazesize) {
+    //storing values based on the values provided
     mover = new SDL_Rect{x, y, width, height};
     texturePath=mtexturePath;
     playertexture.texture=playertexture.load(renderer, texturePath);
     xJump = SCREEN_HEIGHT/mazesize; 
     yJump = SCREEN_HEIGHT/mazesize; 
-    curr_x=mazesize-1;
+    curr_x=mazesize-1; //bottomright position start point
     curr_y=mazesize-1;
 
 }
@@ -17,25 +18,25 @@ Player::~Player() {
     delete mover;
     mover=nullptr;
 }
-
+//moves player using yjump and xjump
 void Player::moveSouth() {
-    if (mover->y + yJump < SCREEN_HEIGHT)
+    if (mover->y + yJump <= SCREEN_HEIGHT)
         mover->y += yJump;
 }
 
 void Player::moveNorth() {
-    if (mover->y - yJump > 0)
+    if (mover->y - yJump >= 0)
         mover->y -= yJump;
     else
         mover->y = 0;
 }
 
 void Player::moveEast() {
-    if (mover->x + xJump < SCREEN_WIDTH)
+    if (mover->x + xJump <= SCREEN_WIDTH)
         mover->x += xJump;
 }
 
 void Player::moveWest() {
-    if (mover->x - xJump > 0)
+    if (mover->x - xJump >= 0)
         mover->x -= xJump;
 }
