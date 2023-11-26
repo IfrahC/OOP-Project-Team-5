606a7e6f10b02ce7394654b01a06dee3704340ac
eshal5 <122152840+eshal5@users.noreply.github.com>
2023-11-26 21:16:52 +0500

    Update score.cpp

--- score.cpp
+++ score.cpp
@@ -6,7 +6,9 @@ Score::Score() : score{0} {}
 int Score::getscore() const {
     return score;
 }
-
+int Score::setscore(int a){
+    score=a;
+}
 void Score::incrementscore(int n) {
     score+=n;
 }
