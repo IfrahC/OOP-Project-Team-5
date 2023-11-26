460162710aa50455b12d2c971dd6da3cc3105fa2
eshal5 <122152840+eshal5@users.noreply.github.com>
2023-11-26 21:06:32 +0500

    Update Player.hpp

--- Player.hpp
+++ Player.hpp
@@ -6,25 +6,27 @@
 #include <SDL.h>
 #include "Texture.hpp"
 #include <string>
-#include "game.hpp"
+
 
 class Player {
 public:
     void addPlayer(SDL_Renderer* renderer, std::string mtexturePath, int x, int y, int width, int height,int scale);
     ~Player();
-    Player()=default;
+    Player()=default; 
+    //move functions for player
     void moveSouth();
     void moveNorth();
     void moveEast();
     void moveWest();
     const int SCREEN_WIDTH=800;
     const int SCREEN_HEIGHT=800;
-
+    
     std::string texturePath;
+
     SDL_Rect* mover;
-    Texture playertexture;
-    int xJump, yJump; // Adjust these values according to your game's needs
-    int curr_x;
+    Texture playertexture;//texture for player
+    int xJump, yJump; // scale for how much the player is to be moved
+    int curr_x; //current x and y positions
     int curr_y;
 };
 #endif
