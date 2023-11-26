fa3188a09224b51be324d42a15213f7dd7b7fe98
eshal5 <122152840+eshal5@users.noreply.github.com>
2023-11-26 20:43:14 +0500

    Update Level.hpp

--- Level.hpp
+++ Level.hpp
@@ -1,23 +1,22 @@
 // Level.h
 #pragma once
-#ifndef LEVEL_H
-#define LEVEL_H
+#ifndef LEVEL_HPP
+#define LEVEL_HPP
 
 class Level {
 public:
     enum Difficulty {
         Easy,
         Medium,
-        Hard,
-        Hardest
+        Hard
     };
 
-    Level(Difficulty difficulty);
-    int getMazeSize() const;
-
+    Level(Difficulty difficulty); //constructor that takes difficulty and assigns maze size
+    int getMazeSize() const; //getter for maze size
+    void setMazeSize(Difficulty difficulty); //setter
 private:
     int mazeSize;
-    void setMazeSize(Difficulty difficulty);
+   
 };
 
-#endif // LEVEL_H
+#endif // LEVEL_HPP
