b70c23e64975286c65eb6ddf3efc448babe9ef43
eshal5 <122152840+eshal5@users.noreply.github.com>
2023-11-26 20:55:19 +0500

    Update MazeRunner.hpp

--- MazeRunner.hpp
+++ MazeRunner.hpp
@@ -3,12 +3,10 @@
 #include<string>
 #include "Cell.hpp"
 #include "Score.hpp"
-#include "Maze.h"
+#include "Maze.hpp"
 #include "game.hpp"
 #include "Player.hpp"
 using namespace std;
 
-extern Score score;
-extern string gameState;
-
+//function that deals with the movement of the player
 std::string makeMove(string direction,Maze* generator,Player& boy,Score& points,std::string gameState);
