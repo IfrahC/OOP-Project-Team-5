c1f0621af751b13ef9a2243b1f0a64b9e2572f0d
eshal5 <122152840+eshal5@users.noreply.github.com>
2023-11-26 20:24:01 +0500

    Update game.hpp

--- game.hpp
+++ game.hpp
@@ -6,36 +6,46 @@
   #include <SDL_ttf.h>
 
 #include "Cell.hpp"
-#include "Maze.h"
-
+#include "Maze.hpp"
+#include "Music.hpp"
 #include "Texture.hpp"
-
+#include "Stopwatch.hpp"
 #include <stdio.h>
 #include <iostream>
 #include <string>
 #include <stdlib.h>
 #include <time.h>
+#include "Player.hpp"
 #include "Score.hpp"
+#include "GameManager.hpp"
 #ifndef GAME_HPP
 #define GAME_HPP
 class Game {
  
-    const int SCREEN_WIDTH = 800;
+    const int SCREEN_WIDTH = 800; //screen dimensions
     const int SCREEN_HEIGHT = 800;
-    Score points;
+
+    Score points; //score of the game
+
+    Stopwatch stopwatch; //stopwatch
+
     // The window we'll be rendering to
     SDL_Window *gWindow = NULL;
     // The window renderer
     SDL_Renderer *gRenderer = NULL;
 
+    GameManager m;
  
-    Maze* generator;
-    /*SDL_Texture* titleTexture;  
-    SDL_Texture* mazebgTexture;*/
-    Texture titleTexture;
+    Maze* generator; //the maze
+    //music
+    Music backgroundMusic;
+    Music collectiblemusic;
+
+    Texture titleTexture; //textures required to draw the maze and players etc
     Texture mazebgTexture;
     Texture selectplayer;
-    //
+    Texture selectlevel;
+    Texture scoredisplay;
     Texture vwall;
     Texture hwall;
     Texture boy;
@@ -45,8 +55,8 @@ public:
     bool init();
     bool loadMedia();
     void close();
-   // SDL_Texture *loadTexture(std::string path);
     void run();
+    ~Game();
 };
 
 #endif // GAME_HPP
